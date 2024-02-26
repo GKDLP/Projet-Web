61b489ed76a1d7dace22c65292ef30ebb655dbb1
GKDLP <155960694+GKDLP@users.noreply.github.com>
2024-02-26 14:30:38 +0100

    Add files via upload

---
+++ page.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./page.css">
+</head>
+<body>
+    <section class="top-page">
+        <header class="entete">
+            <img class="icon" src="./anonymous-avatar-icon-25.webp" alt="icone">
+            <div class="identity">
+                <h1> NOM :</h1>
+                <h1> Adresse Mail:</h1>
+            </div>
+        </header>
+    </section>
+    <div class="Bio">
+    <h2 >Biographie:</h2>
+    </div>
+    <section>
+    <div class>
+    <h2>Projets:</h2>
+    <table border="1">
+        <tr>
+            <td>Nom</td>
+            <td>Description</td>
+            <td>Categorie</td>
+        </tr>
+        <tr>
+            <td>Borne d'arcade</td>
+            <td> jeu super </td>
+            <td>Open innovation</td>
+        </tr>
+        <tr>
+            <td>Cyberneo</td>
+            <td>super interessant</td>
+            <td>workshop</td>
+        </tr>
+    </table>
+    </div>
+</section> 
+</body>
+</html>
+<?php 
+$nom = "Nom";
+$mail= "Email";
+
+
+
+
+
+?>
